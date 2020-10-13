a5b9ca2593df7c4531eee9f373a2a0a1714326ef
NaotoGon <naotogon5710@gmail.com>
2020-10-13 12:20:44 +0900

    配列

---
+++ training_2.php
@@ -0,0 +1,23 @@
+<?php
+
+//配列
+$array1 = [
+  "aaa",
+  "bbb",
+  "ccc",
+];
+$array1[] = "ddd";
+echo $array1[3];
+
+echo "\n";
+
+//連想配列
+$array2 = [
+  "fruit" => "りんご",
+  "car" => "レクサス",
+];
+$array2["flower"] = "パンジー";
+
+echo $array2["flower"]
+
+?>
